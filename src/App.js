50ded94f970e48e70c36fcdcded102381d63329f
Anmol Babbar <anmolbbr08@gmail.com>
2021-04-13 14:03:54 +0530

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from from our shop page. Also included our withSpinner HOC

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+
 class App extends React.Component {
   unsubscribeFromAuth=null;
 
@@ -29,7 +30,6 @@ class App extends React.Component {
         });
       });
       }
-      setCurrentUser(userAuth);
     });
   }
 
