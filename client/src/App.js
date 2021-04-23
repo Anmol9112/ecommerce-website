595b237323d9cfea4de1d71a97b5165dc6237d58
Anmol Babbar <anmolbbr08@gmail.com>
2021-04-24 00:54:06 +0530

    adding performance optimizations to app using lazy, suspence

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import CheckoutPage from './pages/checkout/checkout.component';
+// import HomePage from './pages/homepage/homepage.component';
+// import ShopPage from './pages/shop/shop.component';
+// import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+// import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
+import { GlobalStyle } from './global.styles';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import {checkUserSession} from './redux/user/user.actions';
 
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'))
+const ShopPage = lazy(() => import('./pages/shop/shop.component'))
+const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'))
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'))
+
+ 
 class App extends React.Component {
   unsubscribeFromAuth=null;
 
@@ -27,13 +36,20 @@ class App extends React.Component {
   render()  {
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch> 
+        <ErrorBoundary>
+        <Suspense fallback={<div>...Loading</div>}>
       <Route exact path='/' component={HomePage}/>
+      
       <Route  path='/shop' component={ShopPage}/>
       <Route  path='/checkout' component={CheckoutPage}/>
       <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)}/>
+      </Suspense>
+      </ErrorBoundary>
       </Switch>
+      
     </div>
   );
   }
--- client/src/components/collection-item/collection-item.styles.jsx
+++ client/src/components/collection-item/collection-item.styles.jsx
@@ -19,6 +19,18 @@ export const CollectionItemContainer = styled.div`
       display: flex;
     }
   }
+  @media screen and (max-width: 800px){
+    width: 40vw;
+    &:hover {
+      .image {
+        opacity: unset;
+      }
+  
+      button {
+        opacity: unset;
+      }
+    }
+}
 `;
 
 export const AddButton = styled(CustomButton)`
@@ -27,6 +39,13 @@ export const AddButton = styled(CustomButton)`
   position: absolute;
   top: 255px;
   display: none;
+
+  @media screen and (max-width: 800px) {
+    display: block;
+    opacity: 0.9;
+    min-width: unset;
+    padding: 0 10px 0 10px;
+}
 `;
 
 export const BackgroundImage = styled.div`
@@ -40,7 +59,7 @@ export const BackgroundImage = styled.div`
 
 export const CollectionFooterContainer = styled.div`
   width: 100%;
-  height: 5%;
+  height: 10%;
   display: flex;
   justify-content: space-between;
   font-size: 18px;
